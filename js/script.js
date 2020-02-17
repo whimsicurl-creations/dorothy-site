26ca3dd60ea37ccf2847debd295b2f3c71b4d047
Bethany <blmeier@gmail.com>
2020-02-17 10:40:57 -0500

    Add root style variables, add current year with JS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const overlay = document.querySelector('.overlay');
 const overlayImage = overlay.querySelector('img');
 const overlayClose = overlay.querySelector('.close');
+const year = document.getElementById('year');
 
 //Open image modal on click
 function handleClick(e) {
@@ -23,3 +24,7 @@ images.forEach(image => image.addEventListener('click', handleClick));
 
 //Add event listener to close modal on button click
 overlayClose.addEventListener('click', close);
+
+//Set copyright year using JS
+var currentYear = new Date();
+year.innerHTML = currentYear.getFullYear();
