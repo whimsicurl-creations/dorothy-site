01e099ea62f6d74dd28ed1bc4079ae88fee81b66
Bethany <blmeier@gmail.com>
2020-02-17 10:19:25 -0500

    Add image modal with JS

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//Set variables
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+//Open image modal on click
+function handleClick(e) {
+    const src = e.currentTarget.querySelector('img').src;
+    overlayImage.src = src;
+    overlay.classList.add('open');
+}
+
+//Close modal on click
+function close() {
+    overlay.classList.remove('open');
+}
+
+//Set variable for each image
+const images = document.querySelectorAll('.portfolio-item');
+
+//Add event listener to open modal on click
+images.forEach(image => image.addEventListener('click', handleClick));
+
+//Add event listener to close modal on button click
+overlayClose.addEventListener('click', close);
